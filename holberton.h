1552221bded1f7166a489e3ce2e3435c0b08281d
oscargomez87 <oscarstiven87@gmail.com>
2019-10-29 14:14:50 +0000

    Added header for project and excluded it from git ignore

---
+++ holberton.h
@@ -0,0 +1,24 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdarg.h>
+
+/**
+ * struct frm - struct with format specifiers
+ *
+ *@name: format identifier
+ *@f: function to call
+ */
+typedef struct frm
+{
+	char *name;
+	int (*f)(va_list);
+} form_t;
+int _printf(const char *, ...);
+int _printc(va_list);
+int _prints(va_list);
+
+#endif
