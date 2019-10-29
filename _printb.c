6e21bd04f4ed234c911f4d89f81ad9566149d650
oscargomez87 <oscarstiven87@gmail.com>
2019-10-29 18:29:18 +0000

    Format fixes

--- _printb.c
+++ _printb.c
@@ -1,16 +1,29 @@
 #include "holberton.h"
 int _recprintb(unsigned int n);
 
+/**
+ * _printb - receives argument to convert and print to binary
+ *
+ * @arg: list with int to convert
+ * Return: number of characters printed
+ */
 int _printb(va_list arg)
 {
-	unsigned n = va_arg(arg, unsigned int);
+	unsigned int n = va_arg(arg, unsigned int);
 
 	return (_recprintb(n));
 }
 
+/**
+ * _recprintb - receives unsigned integer to convert and print to binary
+ *
+ * @n: number to convert and print
+ * Return: number of characters printed
+ */
 int _recprintb(unsigned int n)
 {
 	char mod = '0';
+
 	if (n == 0)
 		return (0);
 	mod = (n % 2) + '0';
